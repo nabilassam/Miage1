d9cd277e8af3a90a8adfb795a5ab8430889d5e95
ASSAM <nabilassam1@gmail.com>
2023-09-19 10:57:53 +0200

    premier dessin de la case et du labyrinthe

--- SVG/generateur.java
+++ SVG/generateur.java
@@ -2,9 +2,41 @@ package SVG;
 
 import models.Case;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class generateur {
-   public void generer(Case[] c ){
+   public void generer(Case[] c,int longeur ){
+      StringBuilder svgContent = new StringBuilder();
+      svgContent.append("<svg xmlns='http://www.w3.org/2000/svg' width='400' height='400'>");
+      // Dessinez les éléments du labyrinthe
+      drawLabyrinth(svgContent);
+
+      // Fin du document SVG
+      svgContent.append("</svg>");
+
+      try {
+         FileWriter fileWriter = new FileWriter("labyrinth.svg");
+         fileWriter.write(svgContent.toString());
+         fileWriter.close();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
 
    }
 
+   private static void drawLabyrinth(StringBuilder svgContent) {
+      // Dessinez les murs du labyrinthe (exemple)
+      for (int x = 0; x < 10; x++) {
+         svgContent.append("<rect x='" + 0 + "' y='" + 0 + "' width='" + 1000 + "' height='" + 1000 + "' fill='white' />");
+         for (int y = 0; y < 10; y++) {
+            // Dessinez un mur à chaque coordonnée
+            if (x % 2 == 0 || y % 2 == 0) {
+
+               svgContent.append("<line x1='" + x + "' y1='" + y + "' x2='" + x+2 + "' y2='" + y+2 + "' stroke='black' />");
+            }
+         }
+      }
+   }
 }
+
